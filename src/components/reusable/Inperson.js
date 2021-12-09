a2caf936c7e9b072f60033c926a9a2b29dc04d88
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2021-12-08 23:04:03 -0800

    put layout for gallery page

---
+++ src/components/reusable/Inperson.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../../App.css'
+
+export class Inperson extends Component {
+    render() {
+        return (
+            <div>
+                <div className="inperson">something</div>
+            </div>
+        )
+    }
+}
+
+export default Inperson
