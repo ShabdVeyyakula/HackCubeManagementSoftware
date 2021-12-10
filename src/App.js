e69fd6d873d3b6692580db7ac32b9a40bcd90e97
Pranav Krishna <krishnatechpranav@gmail.com>
2021-12-09 18:10:32 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
 
@@ -20,11 +21,102 @@ import firebase from '../src/firebase/init'
 
 
 export default function App() {
+
+  function constructor(props) {
+    super(props);     
+    this.state = {
+        usernameLogin: '',
+        passwordLogin: "",
+        usernameSignup: "",
+        passwordSignup: "",
+        texts: []
+    };
+
+    this.handleChangeLoginUsername = this.handleChangeLoginUsername.bind(this);
+    this.handleChangeLoginPassword = this.handleChangeLoginPassword.bind(this);
+
+    this.handleChangeSignupUsername = this.handleChangeSignupUsername.bind(this);
+    this.handleChangeSignupPassword = this.handleChangeSignupPassword.bind(this);
+
+    this.login = this.login.bind(this);
+    this.createFirebaseUser = this.createFirebaseUser.bind(this);
+
+
+}
+
+  function handleChangeLoginUsername(event) {  this.setState({usernameLogin:  event.target.value})  }
+
+  function handleChangeLoginPassword(event) {  this.setState({passwordLogin:  event.target.value})  }
+
+  function handleChangeSignupUsername(event) {  this.setState({usernameSignup:  event.target.value})  }
+
+  function handleChangeSignupPassword(event) {  this.setState({passwordSignup:  event.target.value})  }
+
+
   console.log("hello");
+
+  function createFirebaseUser(){
+
+    if(this.state.usernameSignup != "" && this.state.passwordSignup != ""){
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, 'test@gmail.com', 'password123')
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(error)
+          // ..
+        });
+    } else {
+      console.log("Fields are empty")
+    }
+
+    
+  }
+
+
+  function login(){
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, 'test@gmail.com', 'password123')
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("LOGIN SUCCESS")
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error)
+      });
+}
 
   return (
     <Router>
       <div>
+
+        <input value = {this.state.usernameLogin} onChange = {handleChangeLoginUsername}/>
+        <input value = {this.state.passwordLogin} onChange = {handleChangeLoginPassword}/>
+
+
+        <button onClick = {login}>Login</button>
+
+
+        <input value = {this.state.usernameSignup} onChange = {handleChangeSignupUsername}/>
+        <input value = {this.state.passwordSignup} onChange = {handleChangeSignupPassword}/>
+
+
+
+        <button onClick = {createFirebaseUser}>Create Creds</button>
+
+
         <nav>
           
         </nav>
