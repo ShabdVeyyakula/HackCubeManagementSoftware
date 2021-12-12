4dad75aa2807d1a9341331c3b77ea2dbf6b1cd14
Pranav Krishna <krishnatechpranav@gmail.com>
2021-12-11 23:02:21 -0800

    gallery

--- src/components/pages/gallery.js
+++ src/components/pages/gallery.js
@@ -5,8 +5,43 @@ import Rightbar from '../navbars/rightbar'
 import 'bootstrap/dist/css/bootstrap.css';
 import Topbar from '../navbars/Topbar';
 import Galleryitem from '../reusable/Galleryitem';
+import { getAuth } from "firebase/auth";
 
 export class gallery extends Component {
+
+    constructor(props) {
+        super(props);     
+        this.state = {
+            name: "",
+        }
+    }
+    
+
+    getUser(){
+        const auth = getAuth();
+        const user = auth.currentUser;
+        if (user !== null) {
+            // The user object has basic properties such as display name, email, etc.
+            const displayName = user.displayName;
+            const email = user.email;
+            const photoURL = user.photoURL;
+            const emailVerified = user.emailVerified;
+
+            // The user's ID, unique to the Firebase project. Do NOT use
+            // this value to authenticate with your backend server, if
+            // you have one. Use User.getToken() instead.
+            const uid = user.uid;
+            this.state.name = displayName;
+            this.setState({})
+        }
+
+    }
+
+    componentDidMount() {
+        // your source code to load initial data
+        this.getUser();
+    }
+
     render() {
         return (
             <div>
@@ -14,18 +49,24 @@ export class gallery extends Component {
                     <Leftbar />
                     <div className = "centerSection">
                         <Topbar />
-                            <div className ="Row">
-                                <Galleryitem />
+                            <div className ="row">
+
+                                <div className = "col-md-3">
+                                    <Galleryitem />
+                                </div>
+
+                                <div className = "col-md-3">
+                                    <Galleryitem />
+                                </div>
+
+                                <div className = "col-md-3">
+                                    <Galleryitem />
+                                </div>
+
                             </div>
-
-
-
                     </div>
 
-                    <Rightbar />
-
-
-
+                    <Rightbar name = {this.state.name}/>
 
                 </div>
             </div>
